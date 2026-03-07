807e40487966eaa4a79e23e86bcf9478f5046da9
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-03-07 18:55:00 +0100

    Trying to load TEST PAT token from environment

--- src/test/java/TestUtils.java
+++ src/test/java/TestUtils.java
@@ -3,6 +3,10 @@ import java.nio.file.Path;
 
 public class TestUtils {
     public static String getToken() {
+        String token = System.getenv("PAT_TOKEN");
+        if (token != null)
+            return token;
+
         try {
             return Files.readString(Path.of(".env"));
         } catch (Exception _) {}
