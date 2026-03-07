858905486679682420fc9657f0f9ed2d254d0797
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-03-07 01:40:44 +0100

    Made IssueReporter Useful

---
+++ src/main/java/dk/tij/jissuesystem/utils/DeviceUtils.java
@@ -0,0 +1,18 @@
+package dk.tij.jissuesystem.utils;
+
+public final class DeviceUtils {
+    private DeviceUtils() {}
+
+    public static String getDiagnostics() {
+        return String.format("""
+        \n
+        ---
+        **Environment Info:**
+        * **OS:** %s (%s)
+        * **Java:** %s (%s)
+        """,
+                System.getProperty("os.title"), System.getProperty("os.arch"),
+                System.getProperty("java.version"), System.getProperty("java.vendor")
+        );
+    }
+}
